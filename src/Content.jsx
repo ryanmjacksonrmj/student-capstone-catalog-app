c8cc43d5bcfe5d45f950ecd72fd00898ad4f15ee
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-12-17 13:28:39 -0500

    creates show action for capstones in modal

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,26 @@
+import axios from "axios";
+import { CapstonesShow } from "./CapstonesShow";
+import { useState } from "react";
+import { Modal } from "./Modal";
+
 export function Content() {
+  const [isCapstonesShowVisible, setIsCapstonesShowVisible] = useState(false);
+  const [currentCapstone, setCurrentCapstone] = useState({});
+
+  const handleShowCapstone = (capstone) => {
+    setIsCapstonesShowVisible(true);
+    setCurrentCapstone(capstone);
+  };
+
+  const handleClose = () => {
+    setIsCapstonesShowVisible(false);
+  };
   return (
-  <div>
-    <h1>Placeholder</h1>
-  </div>
-  )
+    <div>
+      <h1>Placeholder</h1>
+      <Modal show={isCapstonesShowVisible} onClose={handleClose}>
+        <CapstonesShow capstone={currentCapstone} />
+      </Modal>
+    </div>
+  );
 }
