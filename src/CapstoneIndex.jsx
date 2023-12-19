262819fcc2b21eb4f21813f11073b3d99e83fc78
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-12-18 19:30:46 -0500

    Changes variables to match team schema

--- src/CapstoneIndex.jsx
+++ src/CapstoneIndex.jsx
@@ -4,12 +4,13 @@ export function CapstoneIndex(props) {
       <h1>All capstones</h1>
       {props.capstones.map((capstone) => (
         <div key={capstone.id}>
-          <p>{capstone.capstone_name}</p>
-          <p>{capstone.student_name}</p>
+          <p>{props.capstone.capstone_name}</p>
+          <p>{props.capstone.student_name}</p>
+          <p>{props.capstone.capstone_description}</p>
           <p>
-            <a href={capstone.capstone_url}>Visit the Capstone</a>
+            <a href={props.capstone.capstone_url}>Visit the Capstone</a>
           </p>
-          <img src={capstone.screenshot_url} />
+          <img src={props.capstone.capstone_screenshot_url} />
           <button onClick={() => props.onShowCapstone(capstone)}>More Info</button>
         </div>
       ))}
--- src/CapstonesShow.jsx
+++ src/CapstonesShow.jsx
@@ -4,10 +4,11 @@ export function CapstonesShow(props) {
       <h1>Capstone Info</h1>
       <p>{props.capstone.capstone_name}</p>
       <p>{props.capstone.student_name}</p>
+      <p>{props.capstone.capstone_description}</p>
       <p>
         <a href={props.capstone.capstone_url}>Visit the Capstone</a>
       </p>
-      <img src={props.capstone.screenshot_url} />
+      <img src={props.capstone.capstone_screenshot_url} />
     </div>
   );
 }
